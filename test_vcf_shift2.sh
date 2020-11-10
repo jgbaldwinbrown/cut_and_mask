36e6bb66b36f41fe47cbe3adec3baee569120959
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-11-10 13:57:10 -0700

    more complete shift test

--- test_vcf_shift2.sh
+++ test_vcf_shift2.sh
@@ -5,3 +5,6 @@ set -e
     test.vcf.gz \
     bigmask2.bed \
 > test_masked.vcf.gz
+
+wdiff <(gunzip -c test.vcf.gz) <(gunzip -c test_masked.vcf.gz )  | grep 'scaffold_4_.*375973' > test_pos.txt
+wdiff <(gunzip -c test.vcf.gz) <(gunzip -c test_masked.vcf.gz )  | grep 'scaffold_8_.*12000026' >> test_pos.txt
