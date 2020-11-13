ef55a6bb7a1b710994f6c7ae01f44b09f61068e8
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-11-12 17:18:58 -0700

    uninstall

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+INSTALL_DIR=~/mybin
+
+rm "$INSTALL_DIR/cut_and_mask_vcf2"
+rm "$INSTALL_DIR/cutfasta_maskmiddle"
+rm "$INSTALL_DIR/rename_vcf_chroms"
+rm "$INSTALL_DIR/shift_gff_maskmiddle"
+rm "$INSTALL_DIR/shift_sam_maskmiddle"
+rm "$INSTALL_DIR/shift_vcf_maskmiddle"
