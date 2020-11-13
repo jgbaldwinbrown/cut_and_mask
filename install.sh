717e86d69c7341827b2977a846944376763e6cd4
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-11-12 17:41:32 -0700

    install updated

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 INSTALL_DIR=~/mybin
 
 cp cut_and_mask_vcf2.sh "$INSTALL_DIR/cut_and_mask_vcf2"
+cp cut_mask_shift_rename_vcf.sh "$INSTALL_DIR/cut_mask_shift_rename_vcf"
 cp cutfasta_maskmiddle.py "$INSTALL_DIR/cutfasta_maskmiddle"
 cp rename_vcf_chroms.py "$INSTALL_DIR/rename_vcf_chroms"
 cp shift_gff_maskmiddle.py "$INSTALL_DIR/shift_gff_maskmiddle"
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@
 INSTALL_DIR=~/mybin
 
 rm "$INSTALL_DIR/cut_and_mask_vcf2"
+rm "$INSTALL_DIR/cut_mask_shift_rename_vcf"
 rm "$INSTALL_DIR/cutfasta_maskmiddle"
 rm "$INSTALL_DIR/rename_vcf_chroms"
 rm "$INSTALL_DIR/shift_gff_maskmiddle"
