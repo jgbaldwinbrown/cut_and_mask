3b2954177b205fd441f913a355ba748c2739505d
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-11-12 17:06:20 -0700

    install

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+INSTALL_DIR=~/mybin
+
+cp cut_and_mask_vcf2.sh "$INSTALL_DIR/cut_and_mask_vcf2"
+cp cutfasta_maskmiddle.py "$INSTALL_DIR/cutfasta_maskmiddle"
+cp rename_vcf_chroms.py "$INSTALL_DIR/rename_vcf_chroms"
+cp shift_gff_maskmiddle.py "$INSTALL_DIR/shift_gff_maskmiddle"
+cp shift_sam_maskmiddle.py "$INSTALL_DIR/shift_sam_maskmiddle"
+cp shift_vcf_maskmiddle.py "$INSTALL_DIR/shift_vcf_maskmiddle"
